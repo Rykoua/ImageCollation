c0a142bd6844c1e80ff3ca3236af3597a022e5a9
Ryad <ryad.kaoua@gmail.com>
2021-10-06 12:06:38 +0200

    Added all requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -4,3 +4,4 @@ pip install requests==2.26.0
 pip install fitz==0.0.1.dev2
 pip install PyMuPDF==1.18.14
 pip install bs4==0.0.1
+conda install pytorch==1.8.1 torchvision==0.9.1 torchaudio==0.8.1 cudatoolkit=10.2.89 -c pytorch
